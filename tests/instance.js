7e0a4ed5411841533f6310f26006914951f37fc1
Richard Ayotte <rich@Bluemoon.northpole>
2014-09-23 16:12:40 -0400

    White space

---
+++ tests/instance.js
@@ -0,0 +1,27 @@
+/**
+ * @author Richard Ayotte <rich.ayotte@ayottesoftware.com>
+ */
+
+'use strict';
+
+var advise = require('dcl/advise')
+	, User = require('../models').User
+;
+
+var user = {
+    name: 'Fred Flinstone'
+    , title: 'Stonemaster'
+};
+
+User.create(user)
+.then(function(newUser){
+    advise.around(newUser, 'save', function(save) {
+        return function() {
+            newUser.name = 'Rocker';
+            return save.apply(this, arguments);
+        };
+    });
+
+    newUser.name = 'Rich';
+    newUser.save();
+});
